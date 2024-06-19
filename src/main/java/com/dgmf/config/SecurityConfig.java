6a6011e5555a908d9d2d5d4f8c3b99b0a0c96911
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-19 09:17:15 +0200

    In Memory Authentication

--- src/main/java/com/dgmf/config/SecurityConfig.java
+++ src/main/java/com/dgmf/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -38,5 +42,24 @@ public class SecurityConfig {
 
         // Returns an Object of SecurityFilterChain Type
         return http.build();
+    }
+
+    // In Memory Authentication
+    // InMemoryUserDetailsManager ==> Implementation of UserDetailsService
+    // Prefix {noop} ==> To Tell Spring this Password Should Be Stored as
+    // Plain Text
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}userPassword")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminPassword")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin); // Constructor
     }
 }
